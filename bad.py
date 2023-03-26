d378cff73fbb93baffa7480961998f8b7d4a3132
matthew <stidmatt@gmail.com>
2023-03-26 09:04:37 -0400

    fix_broken: Initial commit bad.py: final logic test
    Signed-off-by: matthew <stidmatt@gmail.com>

--- bad.py
+++ bad.py
@@ -10,5 +10,10 @@ def main():
     print([x for x in entries[1] if list(x.keys())[0] == 'author'][0]['author'])
     print([x for x in entries[0] if list(x.keys())[0] == 'book'][0]['book'])
 
+    for entry in entries:
+        book = [x for x in entry if list(x.keys())[0] == 'book']
+        if book[0]['book'] == 'Dune':
+            print([x for x in entry if list(x.keys())[0] == 'author'][0]['author'])
+
 if __name__ == "__main__":
    main()
---
+++ fix_broken.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import json
+from collections import ChainMap
+
+def dict_merger(entries):
+    result = dict()
+    for entry in entries:
+        for d in entry:
+            if d in result.keys():
+                if type(result[d]) == list:
+                    result[d].append(result[d])
+                else:
+                    result[d] = [result[d]]+[entry[d]]
+            else:
+                result[d] = entry[d]
+    return result
+
+def main():
+    d = json.load(open('bad_api.json'))
+    entries = list()
+    for entry in d['entries']:
+        entries.append(dict_merger(entry))
+    
+    print(entries)
+    print(entries[1]['author'])
+    print(entries[0]['book'])
+
+    for x in entries:
+        if x['book'] == 'Dune':
+            print(x['author'])
+    
+
+
+if __name__ == "__main__":
+   main()
